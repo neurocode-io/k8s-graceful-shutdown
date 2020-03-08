ad472c9e2c41f0548deed917e22c6470090aeb5e
Ziad Shaban <ziad.shaban@iis.fraunhofer.de>
2020-03-08 13:39:57 +0100

    missing: readme, documentation and probably some npm scripts

---
+++ index.ts
@@ -0,0 +1,75 @@
+import { IncomingMessage, ServerResponse } from 'http'
+
+interface HealthHandlerOptions {
+  /**
+   * Health test
+   * Returns true if test passed
+   * Retuns false if test failed
+   */
+  test?: (req: IncomingMessage, res: ServerResponse) => boolean
+  /**
+   * Call back to be triggered if healthy
+   */
+  healthy: (req: IncomingMessage, res: ServerResponse) => void
+  /**
+   * Call back to be triggered if not heathy
+   */
+  notHealthy: (req: IncomingMessage, res: ServerResponse) => void
+}
+
+const hooks = new Map<(...args: any[]) => void, (...args: any[]) => void>()
+const signals = ['SIGINT', 'SIGTERM', 'SIGUSR2'] as const
+
+const addGracefulShutdownHook = (gracefulPeriodMs: number, gracefulShutdownCB: (...args: any[]) => void) => {
+  const gracefulShutdownHook = () => {
+    setTimeout(() => {
+      gracefulShutdownCB()
+    }, gracefulPeriodMs)
+  }
+
+  hooks.set(gracefulShutdownCB, gracefulShutdownHook)
+  signals.forEach(signal => {
+    process.on(signal, gracefulShutdownHook)
+  })
+}
+
+const removeGracefulShutdownHook = (gracefulShutdownCB: (...args: any[]) => void) => {
+  if (hooks.has(gracefulShutdownCB)) {
+    const gracefulShutdownHook = hooks.get(gracefulShutdownCB)
+    hooks.delete(gracefulShutdownCB)
+    signals.forEach(signal => {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      process.removeListener(signal, gracefulShutdownHook!)
+    })
+  }
+}
+
+const getHealthzHandler = (options: HealthHandlerOptions) => {
+  if (!options.test) {
+    options.test = (req: IncomingMessage, res: ServerResponse) => {
+      return true
+    }
+  }
+  signals.forEach(signal => {
+    process.on(signal, () => {
+      options.test = (req: IncomingMessage, res: ServerResponse) => {
+        return false
+      }
+    })
+  })
+
+  return (req: IncomingMessage, res: ServerResponse) => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      if (options.test!(req, res)) {
+        options.healthy(req, res)
+      } else {
+        options.notHealthy(req, res)
+      }
+    } catch (e) {
+      options.notHealthy(req, res)
+    }
+  }
+}
+
+export { addGracefulShutdownHook, removeGracefulShutdownHook, getHealthzHandler }
---
+++ test/gracefulShutdown.test.ts
@@ -0,0 +1,114 @@
+import assert from 'assert'
+import { addGracefulShutdownHook, removeGracefulShutdownHook, getHealthzHandler } from '../index'
+import { setTimeout } from 'timers'
+import { IncomingMessage, ServerResponse } from 'http'
+import { Socket } from 'net'
+
+const signals = ['SIGINT', 'SIGTERM', 'SIGUSR2'] as const
+let callbackCalled: boolean
+let health: string
+let healthzCheck: (req: IncomingMessage, res: ServerResponse) => void
+
+const testCallback = () => {
+  callbackCalled = true
+}
+
+const healthyCB = () => {
+  health = 'OK'
+}
+
+const notHealthyCB = () => {
+  health = 'not OK'
+}
+
+const req = new IncomingMessage(new Socket())
+const res = new ServerResponse(req)
+
+describe('get healthz handler', () => {
+  it('get healthz handler should return correct health check', () => {
+    healthzCheck = getHealthzHandler({ healthy: healthyCB, notHealthy: notHealthyCB })
+    healthzCheck(req, res)
+    assert.equal(health, 'OK')
+
+    healthzCheck = getHealthzHandler({
+      healthy: healthyCB,
+      notHealthy: notHealthyCB,
+      test: () => {
+        return true
+      },
+    })
+    healthzCheck(req, res)
+    assert.equal(health, 'OK')
+
+    healthzCheck = getHealthzHandler({
+      healthy: healthyCB,
+      notHealthy: notHealthyCB,
+      test: () => {
+        return false
+      },
+    })
+    healthzCheck(req, res)
+    assert.equal(health, 'not OK')
+  })
+})
+
+describe('exit signals test', async () => {
+  process.once('beforeExit', () => {
+    process.stdin.resume()
+  })
+
+  before(() => {
+    addGracefulShutdownHook(0, testCallback)
+    callbackCalled = false
+    health = 'test'
+  })
+
+  afterEach(() => {
+    callbackCalled = false
+    health = 'test'
+  })
+
+  signals.forEach(signal => {
+    it(`it should add graceful shutdown hook on exit signal: ${signal}`, done => {
+      healthzCheck = getHealthzHandler({
+        healthy: healthyCB,
+        notHealthy: notHealthyCB,
+        test: () => {
+          return true
+        },
+      })
+      healthzCheck(req, res)
+      assert.equal(health, 'OK')
+
+      process.once(signal, () => {
+        setTimeout(() => {
+          assert.equal(callbackCalled, true)
+          healthzCheck(req, res)
+          assert.equal(health, 'not OK')
+          done()
+        }, 50)
+      })
+
+      process.kill(process.pid, signal)
+    })
+  })
+})
+
+describe('remove graceful shutdown hooks', () => {
+  before(() => {
+    removeGracefulShutdownHook(testCallback)
+  })
+
+  signals.forEach(signal => {
+    it(`it should remove graceful shutdown hook on exit signal: ${signal}`, done => {
+      process.once(signal, () => {
+        setTimeout(() => {
+          assert.equal(callbackCalled, false)
+          done()
+        }, 50)
+      })
+
+      process.kill(process.pid, signal)
+    })
+  })
+})
